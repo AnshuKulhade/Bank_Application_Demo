b0205080d892f08a1e41bbbca2eb43f1bb7894cf
Anshu Kulhade <80118972+AnshuKulhade@users.noreply.github.com>
2023-02-19 03:45:27 +0530

    Update Bank_Logic.cs

--- Bank_Logic.cs
+++ Bank_Logic.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,14 +27,16 @@ namespace Practice
         }
         public string name
         {
-            get { return _name; }
-            set
-            {
-                if (value != "" &&value !=null && _status == true)
+            
+                get { return _name; }
+                set
                 {
-                    _name = value;
-                }
-            }
+
+                    if (value != "" && value != null && _status == true)
+                    {
+                        _name = value;
+                    }
+                }            
         }
         public int credit_card
         {
@@ -45,7 +47,7 @@ namespace Practice
             get { return _status; }
             set
             {
-                if(_status ==true)
+                if (_status == true)
                     _status = value;
             }
         }
@@ -63,25 +65,32 @@ namespace Practice
                 //{
                 //    Console.WriteLine("Withdrawal Is Restricted");
                 //}
-            }       
+            }
         }
         public void Withdrawal()
         {
-            Console.WriteLine($"Please Enter Your Withdrawal Ammount : ");
-            double wd = double.Parse(Console.ReadLine());
-            Console.WriteLine("You Withdrawal Ammount : " + wd);
-            double bal_limit = 100; 
-            double result = balance - wd;
-            if(result >= bal_limit)
+            try
             {
-                Console.WriteLine("Old Account Balance : " + balance);
-                Console.WriteLine("Success Withdrawal Of : " + result);
-                Console.WriteLine("Please Visit Again.");
+                Console.WriteLine($"Please Enter Your Withdrawal Ammount : ");
+                double wd = double.Parse(Console.ReadLine());
+                Console.WriteLine("You Withdrawal Ammount : " + wd);
+                double bal_limit = 100;
+                double result = balance - wd;
+                if (result >= bal_limit)
+                {
+                    Console.WriteLine("Old Account Balance : " + balance);
+                    Console.WriteLine("Success Withdrawal Of : " + result);
+                    Console.WriteLine("Please Visit Again.");
+                }
+                else
+                {
+                    Console.WriteLine("Restricted Withdrawal Ammount Due To Insufficent Account Balance.");
+                    Console.WriteLine("Please Maintain Minimum 100rs.");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                Console.WriteLine("Restricted Withdrawal Ammount Due To Insufficent Account Balance.");
-                Console.WriteLine("Please Maintain Minimum 100rs.");
+                Console.WriteLine(ex.Message);
             }
         }
 
@@ -89,16 +98,24 @@ namespace Practice
         static void Main()
         {
             Bank_logic GSD = new Bank_logic(101, "Neeraj Nath", 101201, true, 5000.00);
-            if (GSD.status == true)
-            {       if(GSD.balance < 100.00)
-                        Console.WriteLine("Please Add Money To Maintain Acc Balance Should be 500rs. ");
-                Console.WriteLine("Account Is Active: Transection is Active");
-            }
-            else
+            try
             {
+                if (GSD.status == true)
+                {
+                    if (GSD.balance < 100.00)
+                        Console.WriteLine("Please Add Money To Maintain Acc Balance Should be 500rs. ");
+                    Console.WriteLine("Account Is Active: Transection is Active");
+                }
+                else
+                {
 
-                Console.WriteLine("Account Is Deactivate : Contact Your Branch Manager");
-            };
+                    Console.WriteLine("Account Is Deactivate : Contact Your Branch Manager");
+                };
+            }
+            catch(Exception ex1)
+            {
+                Console.WriteLine(ex1.Message);
+            }
             Console.WriteLine($"Welcome To Account Information : \n Customer ID : {GSD.id}\n Customer Name : {GSD.name}\n Customer Credit Card Number : {GSD.credit_card}\n Customer Account Status : {GSD.status} \n Customer Current Balanace : {GSD.balance}");
             //GSD.balance -= 20000;
             //GSD.Withdrawal(200);
